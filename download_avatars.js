52111fc849f10c425418ba5c6df0c05c29b50606
Dan Amiri-Fear <danielowenfear@gmail.com>
2018-12-04 04:03:29 +0000

    adds download_avatars.js & .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,32 @@
+
+
 var request = require('request');
+var secrets = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options ={
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+      'User-Agent': 'request'
+    }
+  };
+
+  request(url, function(error, result, body) {
+    callback(error, body);
+
+  });
+
+}
+
+
+
+
+
+getRepoContributors("jquery", "jquery", function(error, result) {
+  console.log("Errors: ", error);
+  console.log("Result: ", result);
+});
