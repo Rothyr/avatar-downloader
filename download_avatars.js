c5c3a18e7a8f69998fcb77055cec4bfa27b0f833
Dan Amiri-Fear <danielowenfear@gmail.com>
2018-12-04 04:26:27 +0000

    added JSON.parse & avatar_url functionality

--- download_avatars.js
+++ download_avatars.js
@@ -10,23 +10,21 @@ function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'Authorization': 'token ' + secrets.GITHUB_TOKEN,
+      'Authorization': ' token ' + secrets.GITHUB_TOKEN,
       'User-Agent': 'request'
     }
   };
 
-  request(url, function(error, result, body) {
-    callback(error, body);
-
+  request(options, function(error, result, body) {
+    var data = JSON.parse(body);
+    callback(error, data);
   });
 
 }
 
-
-
-
-
 getRepoContributors("jquery", "jquery", function(error, result) {
   console.log("Errors: ", error);
-  console.log("Result: ", result);
+  result.forEach(function(contributor){
+    console.log(contributor.avatar_url);
+  })
 });
