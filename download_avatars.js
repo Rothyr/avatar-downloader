28d97e93ccb7c1a87ae49e2acdf5d67a43c4075d
Dan Amiri-Fear <danielowenfear@gmail.com>
2018-12-04 04:06:11 +0000

    added authorisation token to download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -7,10 +7,10 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, callback) {
-  var options ={
+  var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN,
       'User-Agent': 'request'
     }
   };
